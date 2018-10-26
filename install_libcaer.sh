bda532a4ae268670a4bf48323ee39247b8e25818
Sangil Lee <aeternuslsi@gmail.com>
2018-10-26 15:47:45 +0000

    Update libcaer installation script

--- install_libcaer.sh
+++ install_libcaer.sh
@@ -3,6 +3,9 @@ cd libcaer
 
 sudo apt-get install build-essential cmake pkg-config libusb-1.0-0-dev -y
 
+mkdir build
+cd build
+
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_OPENCV=1 ..
 make
 sudo make install
