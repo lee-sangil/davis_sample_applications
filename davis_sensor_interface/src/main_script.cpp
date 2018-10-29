3b91903aaa4620663b05a13ab6882be6c7941704
Sangil Lee <aeternuslsi@gmail.com>
2018-10-29 18:41:56 +0900

    Add argument parser and display frames

--- davis_sensor_interface/src/main_script.cpp
+++ davis_sensor_interface/src/main_script.cpp
@@ -5,6 +5,12 @@
 #include <fstream>
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
 using namespace std;
 
 static atomic_bool globalShutdown(false);
@@ -22,7 +28,24 @@ static void usbShutdownHandler(void *ptr) {
 	globalShutdown.store(true);
 }
 
-int main(void) {
+int main(int argc, char * argv[]) {
+	boost::program_options::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce a help screen")
+		("event,e", "display events")
+		("frame,f", "display APS frames")
+		;
+
+	boost::program_options::variables_map vm;
+	boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+
+	if(vm.count("help"))
+	{
+		std::cout << "Usage: regex [options]\n";
+		std::cout << desc;
+		return (EXIT_SUCCESS);
+	}
+
 // Install signal handler for global shutdown.
 #if defined(_WIN32)
 	if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
@@ -61,6 +84,15 @@ int main(void) {
 	// Open a DAVIS, give it a device ID of 1, and don't care about USB bus or SN restrictions.
 	libcaer::devices::davis davisHandle = libcaer::devices::davis(1);
 
+	// Let's take a look at the information we have on the device.
+	struct caer_davis_info davis_info = davisHandle.infoGet();
+	std::cout << davis_info.deviceString
+		<< " --- ID: " << davis_info.deviceID 
+		<< ", Master: " << davis_info.deviceIsMaster 
+		<< ", DVS X:" << davis_info.dvsSizeX 
+		<< ", DVS Y: " << davis_info.dvsSizeY 
+		<< ", Logic: " << davis_info.logicVersion << std::endl;
+
 	// Send the default configuration before using the device.
 	// No configuration is sent automatically!
 	davisHandle.sendDefaultConfig();
@@ -72,8 +104,6 @@ int main(void) {
 
 	// Let's turn on blocking data-get mode to avoid wasting resources.
 	davisHandle.configSet(CAER_HOST_CONFIG_DATAEXCHANGE, CAER_HOST_CONFIG_DATAEXCHANGE_BLOCKING, true);
-
-	std::cout << "Started davis ..." << std::endl;
 
 	while (!globalShutdown.load(memory_order_relaxed)) {
 		std::unique_ptr<libcaer::events::EventPacketContainer> packetContainer = davisHandle.dataGet();
@@ -90,22 +120,46 @@ int main(void) {
 				std::shared_ptr<const libcaer::events::PolarityEventPacket> polarity
 					= std::static_pointer_cast<libcaer::events::PolarityEventPacket>(packet);
 
+				cv::Mat cvEvents(davis_info.dvsSizeY, davis_info.dvsSizeX, CV_8UC3, cv::Vec3b{127, 127, 127});
+
 				// Print out timestamps and addresses.
 				for (const auto &evt : *polarity) {
-					int64_t ts = evt.getTimestamp64(*polarity);
 					uint16_t x = evt.getX();
 					uint16_t y = evt.getY();
 					bool pol   = evt.getPolarity();
+
+					if( vm.count("event") ){
+						cvEvents.at<cv::Vec3b>(y, x) = pol? cv::Vec3b{0, 0, 255} : cv::Vec3b{0, 255, 0};
+					}
+				}
+
+				if( vm.count("event") ){
+					cv::imshow("DVS-EVS", cvEvents);
+					cv::waitKey(1);
+				}
+			}
+			else if (packet->getEventType() == FRAME_EVENT) {
+				std::shared_ptr<const libcaer::events::FrameEventPacket> frame
+					= std::static_pointer_cast<libcaer::events::FrameEventPacket>(packet);
+
+				for (const auto &f : *frame) {
+					if (f.getROIIdentifier() != 0) {
+						continue;
+					}
+
+					cv::Mat cvFrame = f.getOpenCVMat(false);
+
+					if( vm.count("frame") ){
+							// Simple display, just use OpenCV GUI.
+							cv::imshow("DVS-APS", cvFrame);
+							cv::waitKey(1);
+					}
 				}
 			}
 		}
 	}
 
 	davisHandle.dataStop();
-
-	// Close automatically done by destructor.
-
-	std::cout << "Stopped logging to file." << std::endl;
 
 	return (EXIT_SUCCESS);
 }
--- davis_sensor_save_events/src/main_script.cpp
+++ davis_sensor_save_events/src/main_script.cpp
@@ -5,6 +5,12 @@
 #include <fstream>
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
 using namespace std;
 
 static atomic_bool globalShutdown(false);
@@ -22,7 +28,25 @@ static void usbShutdownHandler(void *ptr) {
 	globalShutdown.store(true);
 }
 
-int main(void) {
+int main(int argc, char * argv[]) {
+	boost::program_options::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce a help screen")
+		("event,e", "display events")
+		("frame,f", "display APS frames")
+		("quiet,q", "run the exe in quiet mode")
+		;
+
+	boost::program_options::variables_map vm;
+	boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+
+	if(vm.count("help"))
+	{
+		std::cout << "Usage: regex [options]\n";
+		std::cout << desc;
+		return (EXIT_SUCCESS);
+	}
+
 // Install signal handler for global shutdown.
 #if defined(_WIN32)
 	if (signal(SIGTERM, &globalShutdownSignalHandler) == SIG_ERR) {
@@ -58,13 +82,38 @@ int main(void) {
 	}
 #endif
 
+	// Make folders for saving current image
+	std::string folder_name_img = "log/images/";
+
+	std::string folder_remove_command;
+	folder_remove_command = "rm -rf " + folder_name_img;
+	if( system(folder_remove_command.c_str()) ){
+		return (EXIT_FAILURE);
+	}
+
+	std::string folder_create_command;
+	folder_create_command = "mkdir -p " + folder_name_img;
+	if( system(folder_create_command.c_str()) ){
+		return (EXIT_FAILURE);
+	}
+
 	// Open file for writing.
-	std::fstream fEventOutput, fIMUOutput;
-	fEventOutput.open("events.txt", std::fstream::app);
-	fIMUOutput.open("imu.txt", std::fstream::app);
+	std::fstream fEventOutput, fIMUOutput, fImageOutput;;
+	fEventOutput.open("log/events.txt", std::fstream::out);
+	fImageOutput.open("log/images.txt", std::fstream::out);
+	fIMUOutput.open("log/imu.txt", std::fstream::out);
 
 	// Open a DAVIS, give it a device ID of 1, and don't care about USB bus or SN restrictions.
 	libcaer::devices::davis davisHandle = libcaer::devices::davis(1);
+
+	// Let's take a look at the information we have on the device.
+	struct caer_davis_info davis_info = davisHandle.infoGet();
+	std::cout << davis_info.deviceString
+		<< " --- ID: " << davis_info.deviceID 
+		<< ", Master: " << davis_info.deviceIsMaster 
+		<< ", DVS X:" << davis_info.dvsSizeX 
+		<< ", DVS Y: " << davis_info.dvsSizeY 
+		<< ", Logic: " << davis_info.logicVersion << std::endl;
 
 	// Send the default configuration before using the device.
 	// No configuration is sent automatically!
@@ -79,6 +128,10 @@ int main(void) {
 	davisHandle.configSet(CAER_HOST_CONFIG_DATAEXCHANGE, CAER_HOST_CONFIG_DATAEXCHANGE_BLOCKING, true);
 
 	std::cout << "Started logging to file ..." << std::endl;
+
+	char image_index_char[255];
+	std::string image_file_name;
+	unsigned int image_index = 0;
 
 	while (!globalShutdown.load(memory_order_relaxed)) {
 		std::unique_ptr<libcaer::events::EventPacketContainer> packetContainer = davisHandle.dataGet();
@@ -95,6 +148,8 @@ int main(void) {
 				std::shared_ptr<const libcaer::events::PolarityEventPacket> polarity
 					= std::static_pointer_cast<libcaer::events::PolarityEventPacket>(packet);
 
+				cv::Mat cvEvents(davis_info.dvsSizeY, davis_info.dvsSizeX, CV_8UC3, cv::Vec3b{127, 127, 127});
+
 				// Print out timestamps and addresses.
 				for (const auto &evt : *polarity) {
 					int64_t ts = evt.getTimestamp64(*polarity);
@@ -102,7 +157,16 @@ int main(void) {
 					uint16_t y = evt.getY();
 					bool pol   = evt.getPolarity();
 
+					if( vm.count("event") ){
+						cvEvents.at<cv::Vec3b>(y, x) = pol? cv::Vec3b{0, 0, 255} : cv::Vec3b{0, 255, 0};
+					}
+
 					fEventOutput << ts << " " << x << " " << y << " " << pol << std::endl;
+				}
+
+				if( vm.count("event") ){
+					cv::imshow("DVS-EVS", cvEvents);
+					cv::waitKey(1);
 				}
 			}
 			else if (packet->getEventType() == IMU6_EVENT) {
@@ -123,6 +187,36 @@ int main(void) {
 							   << " " << gyroY << " " << gyroZ << std::endl;
 				}
 			}
+			else if (packet->getEventType() == FRAME_EVENT) {
+				std::shared_ptr<const libcaer::events::FrameEventPacket> frame
+					= std::static_pointer_cast<libcaer::events::FrameEventPacket>(packet);
+
+				for (const auto &f : *frame) {
+					if (f.getROIIdentifier() != 0) {
+						continue;
+					}
+
+					cv::Mat cvFrame = f.getOpenCVMat(false);
+					int32_t ts = f.getTimestamp();
+
+					if( vm.count("frame") ){
+							// Simple display, just use OpenCV GUI.
+							cv::imshow("DVS-APS", cvFrame);
+							cv::waitKey(1);
+					}
+
+					sprintf(image_index_char, "%08d", image_index++);
+					image_file_name = folder_name_img + image_index_char + ".png";
+					cv::imwrite(image_file_name, cvFrame);
+
+					if( !vm.count("quiet") ){
+						std::cout << image_file_name << std::endl;
+					}
+
+					fImageOutput << ts << " " << "images/frame_" << image_index_char << ".png" << std::endl;
+				}
+
+			}
 		}
 	}
 
@@ -131,6 +225,7 @@ int main(void) {
 	// Close automatically done by destructor.
 	fEventOutput.close();
 	fIMUOutput.close();
+	fImageOutput.close();
 
 	std::cout << "Stopped logging to file." << std::endl;
 
